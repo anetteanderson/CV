5f7fe940a3ddb06d89bd92b0408e38811d0d01fe
Anette Anderson <example@myemail.com>
2026-04-22 21:20:15 +0300

    add week 13 files

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+const fNameSpan = document.querySelector('#fName');
+const lNameSpan = document.querySelector('#lName');
+const ageSpan = document.querySelector('#age');
+const emailSpan = document.querySelector('#email');
+
 const images = ['1.jpeg', '2.jpeg'];
 const targetImages = document.querySelector('.cv-image img');
 let i = 1;
+
+window.onload = () => {
+    fetch('person.json')
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(data);
+            
+            fNameSpan.textContent = data.fName;
+            lNameSpan.textContent = data.lName;
+            ageSpan.textContent = data.age;
+            emailSpan.textContent = data.email;
+        });
+};
 
 targetImages.addEventListener('click', () => {
     targetImages.style.opacity = '0';
